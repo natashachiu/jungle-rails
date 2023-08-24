e9fa59b1fd9d8d431f0b2a732532f0ce122893ed
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-24 09:11:34 +0800

    order confirmation with order details

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    # render json: @products
   end
 
   def show
     @product = Product.find params[:id]
+    # render json: @product
   end
 
 end
