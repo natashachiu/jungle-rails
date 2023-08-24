8857064364627b7e7a38b6a221adc2fd73834223
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-25 00:10:18 +0800

    display count of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
+# Display a count of how many products are in the database
+# Display a count of how many categories are in the database
