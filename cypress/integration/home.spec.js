dfadbd1d3fe3a48986960c1712e00902cea6c298
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-29 08:14:31 -0400

    home page testing

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,21 @@
+describe('home page', () => {
+
+  it('should render home page', () => {
+    cy.visit('/', {
+      auth: {
+        username: 'Jungle',
+        password: 'book'
+      },
+    }
+    );
+  });
+
+  it('should render products on home page', () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("should render 2 products oh home", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+});
