ec5200223849a42d3397f6fa0b39607e706b8bcf
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-29 09:35:27 -0400

    add product to cart test

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,33 @@
+describe('add to cart', () => {
+
+  it('should render home page', () => {
+    cy.visit('/', {
+      auth: {
+        username: 'Jungle',
+        password: 'book'
+      },
+    });
+  });
+
+  it('should render products on home page', () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("should render 2 products oh home", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("should increase cart by one after add to cart is clicked", () => {
+    cy.visit('/', {
+      auth: {
+        username: 'Jungle',
+        password: 'book'
+      },
+    });
+    cy.contains('My Cart (0)');
+    cy.contains("Add").first().click({ force: true });
+    cy.contains('My Cart (1)');
+
+  });
+
+});
